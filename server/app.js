194856f1a020776b4cb550a734f672d59769c5b3
harishhbaskar <harishhbaskar@gmail.com>
2025-12-08 20:44:43 +0530

    update

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.use((req, res, next) => {
 const corsOptions = {
     origin: [
         "http://localhost:5173",                     // Local Frontend
-        "https://notes-app-nine-mocha.vercel.app"    // Live Vercel Frontend
+        "https://notes-app-nine-mocha.vercel.app/api/v1"    // Live Vercel Frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, 
